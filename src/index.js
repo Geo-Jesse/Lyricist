d6bc47d7a0842f1f27b1d7e2f6b22137167d291f
Shirish Dali <designsbydali@gmail.com>
2016-09-18 12:17:55 -0500

    renamed file PascalCase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import searchBar from './components/searchBar';
+import SearchBar from './components/SearchBar';
 
 import './index.css';
 
