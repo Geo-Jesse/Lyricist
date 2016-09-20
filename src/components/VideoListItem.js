c677b7b5a133e21617b156aa071f3f8273862805
Shirish Dali <designsbydali@gmail.com>
2016-09-20 15:16:42 -0500

    updated

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -4,14 +4,13 @@ class VideoListItem extends Component {
   render() {
     return (
       <div className="col-md-3 col-sm-6 hero-feature">
-                <div className="thumbnail">
-                    <img src={this.props.videoItem.snippet.thumbnails.default.url} />
-                    <div className="caption">
-                        {this.props.videoItem.snippet.title}
-                    </div>
-                </div>
-            </div>
-
+          <div className="thumbnail">
+              <img src={this.props.videoItem.snippet.thumbnails.default.url} />
+              <div className="caption">
+                  {this.props.videoItem.snippet.title}
+              </div>
+          </div>
+      </div>
     )
   }
 }
