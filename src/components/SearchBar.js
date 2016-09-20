dd8952f1975e6eec65f61bf8297dba1b2519febf
Chris Robbins <Jaguar85>
2016-09-20 16:06:51 -0500

    getLyrics method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.getLyrics = this.getLyrics.bind(this)
+  }
+    getLyrics(e) {
+      e.preventDefault()
+      let lyrics = this.refs.songTitle.value
+      let url = "http://localhost:3000/lyrics"
+
+      axios.get(url).then(({ data }) => {
+        console.log({data});
+      })
+    }
+
+
   render(){
     return(
-      <form>
-        <input className="searchBar" type="text" placeholder="enter artist/song here" onKeyUp={(event) => {
+      <form onSubmit={this.getLyrics}>
+        <input className="searchBar" ref="songTitle" type="text" placeholder="enter artist/song here" onKeyUp={(event) => {
           this.props.onSearchTermChanged(event.target.value);
         }} />
         <input className="submitButton" type="submit" />
