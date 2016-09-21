888a10ddb8cf87b9cdb5c314a7aee0371019a7e1
Chris Robbins <Jaguar85>
2016-09-21 11:33:21 -0500

    minor changes

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class VideoListItem extends Component {
   render() {
     return (
-      <div className="col-md-3 col-sm-6 hero-feature">
+      <div onClick={this.props.onVideoSelect.bind(this, this.props.videoItem)} className="col-md-3 col-sm-6 hero-feature">
                 <div className="thumbnail">
                     <img alt="" src={this.props.videoItem.snippet.thumbnails.default.url} />
                     <div className="caption">
@@ -13,6 +13,7 @@ class VideoListItem extends Component {
             </div>
 
     )
+    console.log(VideoListItem);
   }
 }
 
